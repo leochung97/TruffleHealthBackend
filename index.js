ffce17f85d164e70583d8552d4995c48953ed03e
Leo Chung <leo.chung97@gmail.com>
2023-02-13 18:23:17 -0500

    Add comments to index.js for documentation. Add individual tests for each field to ensure valid return types

--- index.js
+++ index.js
@@ -2,13 +2,17 @@ const express = require("express");
 const app = express();
 app.use(express.json());
 
+// Initiate bills array for in-space memory
 let bills = [];
 
+// GET Request - Return all bills
 app.get("/items", (req, res) => {
   res.status(200).json(bills);
 });
 
+// POST Request - Add a new bill only if all required fields are present and fields are valid
 app.post("/items", (req, res) => {
+  // Destructure the request body to get the required fields
   const {
     patientName,
     patientAddress,
@@ -17,6 +21,7 @@ app.post("/items", (req, res) => {
     billAmount,
   } = req.body;
 
+  // If there are any empty fields, we can return an error stating that there are missing items
   if (
     !patientName ||
     !patientAddress ||
@@ -27,6 +32,28 @@ app.post("/items", (req, res) => {
     return res.status(400).json({ error: "Missing items on the medical bill" });
   }
 
+  // Individual tests for each field to ensure that they are valid return types
+  if (typeof patientName !== "string") {
+    return res.status(400).json({ error: "Patient name must be a string" });
+  }
+
+  if (typeof patientAddress !== "string") {
+    return res.status(400).json({ error: "Patient address must be a string" });
+  }
+
+  if (typeof hospitalName !== "string") {
+    return res.status(400).json({ error: "Hospital name must be a string" });
+  }
+
+  if (!dateOfService instanceof Date) {
+    return res.status(400).json({ error: "Date of service must be a date" });
+  }
+
+  if (typeof billAmount !== "number") {
+    return res.status(400).json({ error: "Bill amount must be a number" });
+  }
+
+  // If bill is valid, we can push to bills
   bills.push({
     patientName,
     patientAddress,
@@ -35,12 +62,15 @@ app.post("/items", (req, res) => {
     billAmount,
   });
 
+  // Return message that the bill was successfully added.
   res.status(200).json({ message: "Bill has successfully been added!" });
 });
 
+// Listening on port 3000
 const port = 3000;
 app.listen(port, () => {
-  console.log(`Medical bill upload service is up and listening on port ${port}`);
+  // Log to console that the service is up and running
+  console.log(`Listening on port ${port}`);
 });
 
 module.exports = app;
